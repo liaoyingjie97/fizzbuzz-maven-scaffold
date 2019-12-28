0ad0043401e956a94d620d75cd2a736207abd8c5
liaoyingjie <qx9778@qq.com>
2019-12-28 13:54:18 +0800

    廖英杰-成都项目建设处

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -4,14 +4,14 @@ import static org.fest.assertions.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
     @Test
-    public void should_return_Fizz_given_number_divisible_by_3() {
+    public void should_return_Fizz_given_numzber_divisible_by_3() {
         int number = 0;
         for (; number <= 100; number++) {
             if (number % 3 == 0) {
                 if (number % 5 == 0) {
                     assertThat(FizzBuzz.of(number).equals("FizzBuzz"));
                 } else {
-                    assertThat(FizzBuzz.of(number).equals("Fizee"));
+                    assertThat(FizzBuzz.of(number).equals("Fizz"));
                 }
             } else if (number % 5 == 0) {
                 assertThat(FizzBuzz.of(number).equals("Buzz"));
